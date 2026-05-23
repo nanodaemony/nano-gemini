5f7847b74401ecba69bcf926cebc78d59c112c06
nano <1174520425@qq.com>
2026-05-23 17:33:51 +0800

    删除无用的代码

--- sql/eladmin.sql
+++ sql/eladmin.sql
@@ -18,31 +18,6 @@ SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for tool_alipay_config
--- ----------------------------
-DROP TABLE IF EXISTS `tool_alipay_config`;
-CREATE TABLE `tool_alipay_config` (
-  `config_id` bigint(20) NOT NULL COMMENT 'ID',
-  `app_id` varchar(255) DEFAULT NULL COMMENT '应用ID',
-  `charset` varchar(255) DEFAULT NULL COMMENT '编码',
-  `format` varchar(255) DEFAULT NULL COMMENT '类型 固定格式json',
-  `gateway_url` varchar(255) DEFAULT NULL COMMENT '网关地址',
-  `notify_url` varchar(255) DEFAULT NULL COMMENT '异步回调',
-  `private_key` text DEFAULT NULL COMMENT '私钥',
-  `public_key` text DEFAULT NULL COMMENT '公钥',
-  `return_url` varchar(255) DEFAULT NULL COMMENT '回调地址',
-  `sign_type` varchar(255) DEFAULT NULL COMMENT '签名方式',
-  `sys_service_provider_id` varchar(255) DEFAULT NULL COMMENT '商户号',
-  PRIMARY KEY (`config_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT COMMENT='支付宝配置类';
-
--- ----------------------------
--- Records of tool_alipay_config
--- ----------------------------
-BEGIN;
-COMMIT;
-
--- ----------------------------
 -- Table structure for tool_email_config
 -- ----------------------------
 DROP TABLE IF EXISTS `tool_email_config`;
@@ -85,32 +60,6 @@ CREATE TABLE `tool_local_storage` (
 -- Records of tool_local_storage
 -- ----------------------------
 BEGIN;
-COMMIT;
-
--- ----------------------------
--- Table structure for tool_s3_storage
--- ----------------------------
-DROP TABLE IF EXISTS `tool_s3_storage`;
-CREATE TABLE `tool_s3_storage` (
-  `storage_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `file_name` varchar(255) NOT NULL COMMENT '文件名称',
-  `file_real_name` varchar(255) NOT NULL COMMENT '真实存储的名称',
-  `file_size` varchar(100) NOT NULL COMMENT '文件大小',
-  `file_mime_type` varchar(50) NOT NULL COMMENT '文件MIME 类型',
-  `file_type` varchar(50) NOT NULL COMMENT '文件类型',
-  `file_path` tinytext NOT NULL COMMENT '文件路径',
-  `create_by` varchar(255) NOT NULL COMMENT '创建者',
-  `update_by` varchar(255) NOT NULL COMMENT '更新者',
-  `create_time` datetime NOT NULL COMMENT '创建日期',
-  `update_time` datetime NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`storage_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='s3 协议对象存储';
-
--- ----------------------------
--- Records of tool_s3_storage
--- ----------------------------
-BEGIN;
-INSERT INTO `tool_s3_storage` (`storage_id`, `file_name`, `file_real_name`, `file_size`, `file_mime_type`, `file_type`, `file_path`, `create_by`, `update_by`, `create_time`, `update_time`) VALUES (4, 'tx.jpg', '2ca1de24d8fa422eae4ede30e97c46d8.jpg', '29.67KB', 'image/jpeg', 'jpg', '2025-06/2ca1de24d8fa422eae4ede30e97c46d8.jpg', 'admin', 'admin', '2025-06-25 15:48:22', '2025-06-25 15:48:22');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;

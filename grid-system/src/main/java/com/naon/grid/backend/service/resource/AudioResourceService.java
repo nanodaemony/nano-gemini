777db770e417c6dd4dc30b7ccc096ad2f1f4779e
nano <1174520425@qq.com>
2026-05-30 20:56:23 +0800

    feat: add batch query method for AudioResourceService

--- grid-system/src/main/java/com/naon/grid/backend/service/resource/AudioResourceService.java
+++ grid-system/src/main/java/com/naon/grid/backend/service/resource/AudioResourceService.java
@@ -5,11 +5,15 @@ import com.naon.grid.backend.service.resource.dto.AudioResourceQueryCriteria;
 import com.naon.grid.utils.PageResult;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface AudioResourceService {
 
     Long create(AudioResourceDto resources);
 
     AudioResourceDto findById(Long id);
+
+    List<AudioResourceDto> findByIds(List<Long> ids);
 
     PageResult<AudioResourceDto> queryAll(AudioResourceQueryCriteria criteria, Pageable pageable);
 

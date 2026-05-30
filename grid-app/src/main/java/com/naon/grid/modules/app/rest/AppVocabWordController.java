d6f92fffea28092036d5d3bf7852e9ee679539c9
nano <1174520425@qq.com>
2026-05-30 21:08:34 +0800

    fix: add missing import for RequestMapping

--- grid-app/src/main/java/com/naon/grid/modules/app/rest/AppVocabWordController.java
+++ grid-app/src/main/java/com/naon/grid/modules/app/rest/AppVocabWordController.java
@@ -23,8 +23,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Collections;

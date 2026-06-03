f59db25c870d0d7473bb50b0d5a122918596b6a7
nano <1174520425@qq.com>
2026-06-03 20:46:17 +0800

    feat: add PinyinResponse DTO

---
+++ grid-tools/src/main/java/com/naon/grid/service/dto/PinyinResponse.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright 2019-2025 Zheng Jie
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.naon.grid.service.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 拼音生成响应 DTO
+ * @author nano
+ * @date 2026-06-03
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(description = "拼音生成响应结果")
+public class PinyinResponse {
+
+    @ApiModelProperty(value = "拼音结果", required = true, example = "Nǐ hǎo, wǒ xǐhuan chī mǐfàn")
+    private String pinyin;
+
+    @ApiModelProperty(value = "厂商请求 ID", notes = "由大模型厂商返回的唯一请求标识，用于问题排查")
+    private String requestId;
+
+    @ApiModelProperty(value = "输入 token 数", notes = "用户输入的 token 数量，用于计费")
+    private Integer inputTokens;
+
+    @ApiModelProperty(value = "输出 token 数", notes = "模型输出的 token 数量，用于计费")
+    private Integer outputTokens;
+
+    @ApiModelProperty(value = "总 token 数", notes = "输入和输出的 token 总数，用于计费")
+    private Integer totalTokens;
+
+    @ApiModelProperty(value = "请求耗时", notes = "从收到请求到返回响应的总耗时，单位毫秒")
+    private Long latencyMs;
+}

408b39475252c9ae84adc06b039befbd7dac7312
nano <1174520425@qq.com>
2026-07-04 14:53:45 +0800

    feat: add SendCodeDTO for email verification code request

---
+++ grid-app/src/main/java/com/naon/grid/modules/app/service/dto/SendCodeDTO.java
@@ -0,0 +1,14 @@
+package com.naon.grid.modules.app.service.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class SendCodeDTO {
+
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确")
+    private String email;
+}

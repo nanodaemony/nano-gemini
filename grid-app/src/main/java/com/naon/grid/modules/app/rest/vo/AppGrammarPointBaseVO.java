6087da25fe49cf6f7d24959ee87c55891837bc70
nano <1174520425@qq.com>
2026-07-06 23:17:06 +0800

    feat: add AppGrammarPointBaseVO

---
+++ grid-app/src/main/java/com/naon/grid/modules/app/rest/vo/AppGrammarPointBaseVO.java
@@ -0,0 +1,30 @@
+package com.naon.grid.modules.app.rest.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class AppGrammarPointBaseVO implements Serializable {
+
+    @ApiModelProperty(value = "语法点ID")
+    private Long id;
+
+    @ApiModelProperty(value = "语法点名称")
+    private String name;
+
+    @ApiModelProperty(value = "HSK等级")
+    private String hskLevel;
+
+    @ApiModelProperty(value = "项目")
+    private String project;
+
+    @ApiModelProperty(value = "类别")
+    private String category;
+
+    @ApiModelProperty(value = "细目")
+    private String subCategory;
+}
